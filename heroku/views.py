3328a2d47a8099da1d7770d65d6ac1b340751107
dayankennedyspiid <dayankennedyspiid@gmail.com>
2022-04-09 11:57:11 +0300

    make it

--- heroku/views.py
+++ heroku/views.py
@@ -5,12 +5,3 @@ def home(request):
     return render(request, 'heroku/home.html')
 
 
-# This is the index page
-def base(request):
-    return render(request, 'heroku/base.html')
-
-
-
-# This the index page
-def index(request):
-    return render(request, 'heroku/index.html')
