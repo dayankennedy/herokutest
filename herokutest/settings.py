8bcde1740d90dbdbefb14606fed7904f4c72d21e
dayankennedyspiid <dayankennedyspiid@gmail.com>
2022-04-16 12:10:48 +0300

    static root added

--- herokutest/settings.py
+++ herokutest/settings.py
@@ -131,7 +131,13 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'heroku/Static/')
 ]
 
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATICFILES_ROOT = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
